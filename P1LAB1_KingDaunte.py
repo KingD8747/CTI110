a4d55861001c6d4ce9d9546e34868aecebb9bd7b
KingD8747 <kingd8747@student.faytechcc.edu>
2025-02-09 15:58:22 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_KingDaunte.py
@@ -0,0 +1,9 @@
+#Daunte King
+#2/9/25
+#P1LAB1
+#Asks for name then introduce you to the class
+print('Enter your first name:', end=' ')
+FirstName= input()
+print('Enter your last name:', end=' ')
+LastName= input()
+print('Hello,',FirstName, LastName,'! Welcome to CTI-110')
